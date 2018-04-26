304af282f48d191df1df075c3c23eff3bf325b6c
vonpupp <albert@haevas.com>
2018-04-25 21:13:05 -0300

    Add bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#DIRNAME=`dirname $0`
+#
+#source $DIRNAME/.vars
+#source ~/.homesick/repos/homeshick/homeshick.sh
+#homeshick link $REPO_NAME
+#
+## PIP
+#source ~/.venv/bin2/bin/activate
+#pip install \
+#    trash \
+#    envdir \
+#    virtualenvwrapper \
+#    #autoenv \
+#    #
+#
+## Autoenv
+#git clone https://github.com/kennethreitz/autoenv.git ~/.autoenv
+#
+## FZF
+#git clone https://github.com/junegunn/fzf.git ~/.fzf
+#~/.fzf/install
+#
+## BASE16
+#git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+# OH-MY-ZSH
+curl -L https://get.oh-my.fish | fish
+
+#chsh -s $(which fish)
