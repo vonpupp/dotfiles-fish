006a1ae05d6bbe21a8490041d8a1eb86424afc40
vonpupp <albert@haevas.com>
2018-09-17 14:16:36 +0200

    Fix ctrl+f for accept-autosuggestion

--- home/.config/fish/config-keybindings.fish
+++ home/.config/fish/config-keybindings.fish
@@ -5,6 +5,9 @@ function fish_user_key_bindings
     # Exit the insert mode on C-Q
     bind -M insert -m default \cq force-repaint
 
+    # Accept auto suggestions
+    bind -M insert -m default \cf accept-autosuggestion
+
     # fzf bindings
     bind -M insert \cr 'fzf-history; commandline -f repaint'
 end
