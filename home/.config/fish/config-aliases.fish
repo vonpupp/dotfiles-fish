b7bf1c1feaa8dc60f0360c37b47cc4ef1b7dfcc7
vonpupp <albert@haevas.com>
2018-10-05 11:32:06 -0300

    Fix conflict

--- home/.config/fish/config-aliases.fish
+++ home/.config/fish/config-aliases.fish
@@ -1,51 +1,48 @@
 # Shell
-abbr -a l ls -lh
-abbr -a ll ls -alh
-abbr -a lls ls -alhSr
-abbr -a lt ls -alhtr
+abbr -a l 'ls -lh'
+abbr -a ll 'ls -alh'
+abbr -a lls 'ls -alhSr'
+abbr -a lt 'ls -alhtr'
 abbr -a psg 'ps aux | ag --smart-case'
+alias mkdir "mkdir -p"
+abbr -a ff 'find . | ag --smart-case'
+abbr -a fd 'find . -type d | ag --smart-case'
+abbr -a fc 'rg --line-number --no-heading --color=always --ignore-case --hidden'
+abbr -a du 'du -hs'
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
 
 # Git
-abbr -a ga git add
-abbr -a gb git branch
-abbr -a gco git checkout
-abbr -a gd git diff
-abbr -a gp git push
-abbr -a gs git status -sb
-abbr -a gu git pull --rebase
+abbr -a ga 'git add'
+abbr -a gb 'git branch'
+abbr -a gco 'git checkout'
+abbr -a gd 'git diff'
+abbr -a gp 'git push'
+abbr -a gs 'git status -sb'
+abbr -a gu 'git pull --rebase'
 
 # Conditional abbreviations
-test -f ~/.local/bin/trash; and abbr -a rm trash
-test -f ~/.local/bin/pydf; and abbr -a df pydf
-test -f /usr/bin/colordiff; and abbr -a diff colordiff
+test -f ~/.local/bin/trash; and abbr -a rm 'trash'
+test -f ~/.local/bin/pydf; and abbr -a df 'pydf'
+test -f /usr/bin/colordiff; and abbr -a diff 'colordiff'
 
 # Sudo abbreviations
-abbr -a pm-suspend sudo pm-suspend
-abbr -a reboot sudo reboot
-abbr -a shutdown sudo shutdown
+abbr -a pm-suspend 'sudo pm-suspend'
+abbr -a reboot 'sudo reboot'
+abbr -a shutdown 'sudo shutdown'
 
-# Misc abbreviations
 abbr -a du du -hs
 abbr -a bc bc -l
 
 # alias llsize='l --group-directories-first -S'
-# alias pbcopy='xsel --clipboard --input'
-# alias pbpaste='xsel --clipboard --output'
-# alias psg='ps aux | grep'
 # alias emacs-restart='pkill emacs; emacs --daemon'
-# alias g='grep -i'
-# alias x='dtrx'
 # alias le='less'
-# alias za='zathura'
-# alias v='vim --servername vim'
-# alias view='vim -R -'
-# alias cpsshkey='xclip -sel clip < ~/.ssh/id_rsa.pub'
-# alias ff='find |grep'
-# alias t='task'
+alias cpsshkey='xclip -sel clip < ~/.ssh/id_rsa.pub'
+alias t='task'
 # alias habit='task rc.data.config=~/.habitwarriorrc rc.data.location=~/.habitwarrior'
-# alias impersonatewm='wmname LG3D'
-# alias pt='ping google.com'
-#
+alias impersonatewm='wmname LG3D'
+abbr -a pt 'ping google.com'
+
 # # fasd aliases
 # alias ee='fasd -ae e'
 # alias ed='fasd -de e'
@@ -53,21 +50,17 @@ abbr -a bc bc -l
 # alias vd='fasd -de vim'
 # alias vf='fasd -fe vim'
 #
-# # Load/reload
-# alias homeshickload='source ~/.homesick/repos/homeshick/homeshick.sh'
-# alias homeshicklink='~/bin/homeshick-link-bb.sh'
-# alias xresourcesload="xrdb ~/.Xresources"
-#
+# Load/reload
+# abbr -a homeshickload source ~/.homesick/repos/homeshick/homeshick.sh
+alias homeshicklink="~/bin/homeshick-link-bb.sh"
+alias xresourcesload="xrdb ~/.Xresources"
+
 # # Node
 # alias npm-exec='PATH=$(npm bin):$PATH'
-#
-# # Latex
-# alias latexmkcompile="grep -l '\\documentclass' *tex | xargs latexmk -pdf -pvc -silent"
-#
+
 # # Django
 # #alias manage='python $VIRTUAL_ENV/../manage.py'
-#
-# # GPG
-# alias gpg=gpg2
-# alias gpg-decrypt='gpg --passphrase-fd 0 --decrypt-files *.gpg'
-# #alias otp='otp -c'
+
+# GPG
+alias gpg="gpg2"
+alias gpg-decrypt="gpg --passphrase-fd 0 --decrypt-files $ARGV"
