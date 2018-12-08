dc8ab2bb92009d136df34accf90c403fc2aead45
vonpupp <albert@haevas.com>
2018-12-07 22:52:33 -0200

    Disable vi keybindings

--- home/.config/fish/config-keybindings.fish
+++ home/.config/fish/config-keybindings.fish
@@ -1,6 +1,6 @@
 # Key bindings
 function fish_user_key_bindings
-    fish_vi_key_bindings
+    # fish_vi_key_bindings
 
     # Exit the insert mode on C-Q
     bind -M insert -m default \cq force-repaint
